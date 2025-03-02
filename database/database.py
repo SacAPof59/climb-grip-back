73fd5be8b1e5415b057f8ef277a93e1d6874ebf0
Hugo <hugo.senechal@ensam.eu>
2025-03-02 18:15:35 +0100

    Bug database

--- database/database.py
+++ database/database.py
@@ -1,7 +1,15 @@
 from sqlalchemy import create_engine, func
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "sqlite:///./.database/data.db"
+DATABASE_DIRECTORY = "database"  # Name of your database directory
+DATABASE_FILE = "data.db"        # Name of your database file
+
+#Construct the full path using the directory and the file name
+DATABASE_URL = f"sqlite:///{os.path.join(DATABASE_DIRECTORY, DATABASE_FILE)}" 
+
+# Create the database directory if it doesn't exist
+os.makedirs(DATABASE_DIRECTORY, exist_ok=True)
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
